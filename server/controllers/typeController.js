98e290df01ba7f99ec4c947237a8efb023edaab6
SemyonSergeev <semyonsergeev30@gmail.com>
2021-12-22 10:48:11 +0300

    Release error processing

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,6 +1,10 @@
+const { Type } = require('../models/models');
+const ApiError = require('../error/ApiError');
 class TypeController {
   async create(req, res) {
-    
+    const { name } = req.body;
+    const type = await Type.create({ name });
+    return req.json(type);
   }
 
   async getAll(req, res) {
